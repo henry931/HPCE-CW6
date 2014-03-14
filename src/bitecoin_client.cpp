67432362fd60a86fb8a404907a0d71a8aa958b2d
Henry Poulton <hgp10@ic.ac.uk>
2014-03-14 17:39:15 +0000

    Henry's Initial commit

--- src/bitecoin_client.cpp
+++ src/bitecoin_client.cpp
@@ -6,8 +6,9 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <unistd.h> 
+#include <unistd.h>
 #include <csignal>
+#include <memory>
 
 int main(int argc, char *argv[])
 {
@@ -15,30 +16,30 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "bitecoin_client client_id logLevel connectionType [arg1 [arg2 ...]]\n");
 		exit(1);
 	}
-	
+
 	// We handle errors at the point of read/write
 	signal(SIGPIPE, SIG_IGN);	// Just look at error codes
 
-	
-	try{		
+
+	try{
 		std::string clientId=argv[1];
 		std::string minerId="David's Miner";
-		
+
 		// Control how much is being output.
 		// Higher numbers give you more info
 		int logLevel=atoi(argv[2]);
 		fprintf(stderr, "LogLevel = %s -> %d\n", argv[2], logLevel);
-		
+
 		std::vector<std::string> spec;
 		for(int i=3;i<argc;i++){
 			spec.push_back(argv[i]);
 		}
-		
+
 		std::shared_ptr<bitecoin::ILog> logDest=std::make_shared<bitecoin::LogDest>(clientId, logLevel);
 		logDest->Log(bitecoin::Log_Info, "Created log.");
-		
-		std::unique_ptr<bitecoin::Connection> connection{bitecoin::OpenConnection(spec)};
-		
+
+		std::unique_ptr<bitecoin::Connection> connection=bitecoin::OpenConnection(spec);
+
 		bitecoin::EndpointClient endpoint(clientId, minerId, connection, logDest);
 		endpoint.Run();
 
@@ -52,7 +53,7 @@ int main(int argc, char *argv[])
 		std::cerr<<"Caught unknown exception."<<std::endl;
 		return 1;
 	}
-	
+
 	return 0;
 }
 
